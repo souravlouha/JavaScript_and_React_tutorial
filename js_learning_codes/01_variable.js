dcf35f708742412d1f7fb4ea6df78ac39a5ba1fd
SOURAV LOUHA <130911872+souravlouha@users.noreply.github.com>
2024-04-13 14:55:42 +0000

    change code

---
+++ js_learning_codes/01_variable.js
@@ -0,0 +1,22 @@
+const accountId =144553
+let accountEmail = "sourav@gail.com"
+var accountPassword= "1234"
+accountCity="kolkata"
+
+
+
+//accountId =458236//
+ accountEmail = "s001@gail.com"
+var accountPassword= "2158"
+accountCity="PUNE"
+
+
+/*
+Prefer not to Use var
+ because of issue in block scope and functional scope
+
+*/
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
