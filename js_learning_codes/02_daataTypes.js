07c6f2b30153b488d5843b2f362c0d2e395e2469
sourav louha <souravlouha00@gail.com>
2024-04-19 23:15:14 +0530

    create a new file

---
+++ js_learning_codes/02_daataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all JS code as newer versions 
+// we are using Node.js ,not browser
+
+// number => 2 to power 53
+// bigint => when we work with so long number then we use bigint . Example=> stock website and  big website ,etc.
+// string => ""
+// boolean => true/false
+//null => (standalone value)
+// undefined => 
+// symbol =>unique 
+//object =>
+
+console.log(typeof "sourav"); // typeof ar sahajee amra console.log ar bhetore ja ache tar type jante pari , jemon ai khane amra "sourav" ar type je string se ta jante parlam .
