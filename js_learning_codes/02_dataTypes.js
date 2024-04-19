83740d130e579b648d2777dc14f6f2f5f66fdd1c
sourav louha <souravlouha00@gail.com>
2024-04-20 00:05:50 +0530

    change

--- js_learning_codes/02_dataTypes.js
+++ js_learning_codes/02_dataTypes.js
@@ -1,6 +1,7 @@
 "use strict"; // treat all JS code as newer versions 
 // we are using Node.js ,not browser
 
+
 // number => 2 to power 53
 // bigint => when we work with so long number then we use bigint . Example=> stock website and  big website ,etc.
 // string => ""
