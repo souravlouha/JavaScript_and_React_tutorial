ca7fe2c03c937cc633788134a0ec9079bcfe637a
sourav louha <souravlouha00@gail.com>
2024-04-19 23:51:43 +0530

    change

--- js_learning_codes/03_conversionOperation.js
+++ js_learning_codes/03_conversionOperation.js
@@ -1,10 +1,13 @@
 let score = 33
 let score2 = "44"
 
-console.log( typeof score);
+console.log( typeof score); // amra ai khane "score" ar "score2" ar type gulo dekhlam/ ba dekhbo . 
 console.log(typeof score2);
 
-let valueInNumber = Number(score)
+// score ar score2 ar modhhe amra Number pass koriyechi but jokhoni amra double code ("") lagabo otkhoni ata Number theke String a poriborton hoye jabe .
+// ai Number theke String a jate poriborton na hoy tar jonnoi ai ( let valueInNumber =Number  ) ar class guloke difine kora hoyeche .
+
+let valueInNumber = Number(score) // 
 let valueInNumber2 = Number(score2)
 
 console.log( typeof valueInNumber);
