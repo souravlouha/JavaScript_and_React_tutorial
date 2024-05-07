d80123ec99986d95aa3cc622524363b32e5e7556
sourav louha <souravlouha00@gail.com>
2024-05-08 02:19:25 +0530

    Update 07_string_in_javascript.js

--- js_codes/07_string_in_javascript.js
+++ js_codes/07_string_in_javascript.js
@@ -1,4 +1,26 @@
 const name = "sourav"
 const repoCount = 10
 
+// ai bhabe amra kono duto string ke aksathe  + sign ar madhhome likhte pari . But ai podhotti ta aktu purono mane outdated , tai amra ata otota use korbo na . 
 console.log(name  + repoCount);
+
+// Tar bodole amra ai sentex ta use korbo , karon ai ta aktu mordan , 
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`${name} has ${repoCount} repos`);
+
+
+
+//To declare variables in JavaScript, you need to use the var, let, or const keyword. Whether it is a string or a number, use the var, let, or const keyword for its declaration. But for declaring a string variable we had to put the string inside double quotes or single quotes.
+
+
+
+
+
+
+
+
+
+
+
+
