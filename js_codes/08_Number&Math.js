598f019de3e0182b317864e413425ed20cfdad1a
sourav louha <souravlouha00@gail.com>
2024-05-08 19:47:31 +0530

    Update 08_Number&Math.js

---
+++ js_codes/08_Number&Math.js
@@ -0,0 +1,23 @@
+const score = 400
+console.log(score);
+
+// amra ai khane score a kichu number lekha hoyeche , but aita number hisebei stor hoyrche ki na , ta amra janina , mane score ar modhhe 400 number hisebe stor hoteo pare kinba nao hote pare .
+
+const balance = new Number(100)
+console.log(balance);
+// ar ai khane , amra dedicatedle aikhane 100 ke number hisebei stor store koriyechi .
+
+
+console.log(balance.toString());
+// ar ai khane ,balance ar type of age chilo number ar akhon ai podhhotir jonne change hoye , string a porinoto hoyeche .
+
+console.log(balance.toString().length); // amra ar madhhome , length check korte parbo string ar .
+
+
+console.log(balance.toFixed(2)); // 2 ghor obdhi 
+console.log(balance.toFixed(4)); // 4 ghor obdhi 
+// ata kono calculation ar answer ar decimal , value koto ghor kinba koto dur obdhi show korate hobe , tar jonne mulot babohar hoye thake .
+
+const otherNumber = 45.23387
+
+console.log(otherNumber.toPrecision(2));
