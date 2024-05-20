f0e9cb82ed0db8c17da8109e69c07df5ff62c2b0
sourav louha <souravlouha00@gail.com>
2024-05-21 00:36:51 +0530

    Create 10_Array.js

---
+++ js_codes/10_Array.js
@@ -0,0 +1,25 @@
+
+const myArr = [0,1, 2, 3, 4, 5]
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+// ai link a sob kichu bistarit bola ache 
+ 
+/* In JavaScript, arrays aren't primitives but are instead Array objects with the following core characteristics:
+
+JavaScript arrays are resizable and can contain a mix of different data types. (When those characteristics are undesirable, use typed arrays instead.)
+JavaScript arrays are not associative arrays and so, array elements cannot be accessed using arbitrary strings as indexes, but must be accessed using nonnegative integers (or their respective string form) as indexes.
+JavaScript arrays are zero-indexed: the first element of an array is at index 0, the second is at index 1, and so on — and the last element is at the value of the array's length property minus 1.
+JavaScript array-copy operations create shallow copies. (All standard built-in copy operations with any JavaScript objects create shallow copies, rather than deep copies).*/
+
+// A shallow copy of an object is a copy whose properties share the same references (point to the same underlying values) as those of the source object from which the copy was made. As a result, when you change either the source or the copy, you may also cause the other object to change too. That behavior contrasts with the behavior of a deep copy, in which the source and copy are completely independent.
+
+// A deep copy of an object is a copy whose properties do not share the same references (point to the same underlying values) as those of the source object from which the copy was made. As a result, when you change either the source or the copy, you can be assured you're not causing the other object to change too. That behavior contrasts with the behavior of a shallow copy, in which changes to nested properties in the source or the copy may cause the other object to change too.
+
+
+
+ const myHeros = ["Thore ","Ironman ", "Hanuman"]
+ const myArr2 = new Array(1,2,3,4)
+ console.log(myArr[1]);
+
+
