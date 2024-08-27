ee59f71ebd1c3bd79c84c2a13754cb76415e3c92
sourav louha <souravlouha00@gail.com>
2024-08-28 03:15:35 +0530

    update

--- js_codes/10_Array.js
+++ js_codes/10_Array.js
@@ -3,7 +3,7 @@ const myArr = [0,1, 2, 3, 4, 5]
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 
-// ai link a sob kichu bistarit bola ache 
+// ai link a sob kichu bistarito bola ache 
  
 /* In JavaScript, arrays aren't primitives but are instead Array objects with the following core characteristics:
 
@@ -20,6 +20,13 @@ JavaScript array-copy operations create shallow copies. (All standard built-in c
 
  const myHeros = ["Thore ","Ironman ", "Hanuman"]
  const myArr2 = new Array(1,2,3,4)
- console.log(myArr[1]);
+ console.log(myArr2[2]);
 
 
+// some methods of arrays
+// Push and Pop 
+myArr.push(6)
+console.log(myArr);
+
+myArr.pop() // pop method use jab hum log karte hai to pop kuch vi nahi dekh kar sabse last wala value delet kardeta hai 
+ console.log(myArr);
