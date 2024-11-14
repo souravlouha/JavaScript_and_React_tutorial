05d90a679290e11ce2a264d91fb6dabd76a01e74
sourav louha <souravlouha00@gail.com>
2024-11-14 22:09:50 +0530

    update

---
+++ All_DSA(main)/demo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Demo {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int x = scanner.nextInt();
+    System.out.println("Hey " + x);
+    scanner.close();
+
+  }
+}
