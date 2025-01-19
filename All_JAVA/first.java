f63e41209cb1c222c3146477ed402a57c40dafb0
sourav louha <souravlouha00@gail.com>
2025-01-20 03:18:42 +0530

    update

--- All_JAVA/first.java
+++ All_JAVA/first.java
@@ -1,7 +1,7 @@
-class Test
-{
-    public static void main(String[] args)
-    {
-        System.out.println("My First Java Program.");
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
     }
 }
